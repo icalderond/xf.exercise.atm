f5efdb7d2c91c891eb27c4be2ab766c23bcc39e1
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-04 22:39:01 -0600

    feat converter: add converter to multiply value and dollar

---
+++ xf.exercise.atm/Views/Converters/TotalBillConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+using xf.exercise.atm.Models;
+
+namespace xf.exercise.atm.Views.Converters
+{
+    public class TotalBillConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return 0;
+            var dollarsBill = (DollarsBill)value;
+            return dollarsBill.Quantity * dollarsBill.Value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
