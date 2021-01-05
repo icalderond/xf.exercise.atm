728ca056c46ebc1332b68d80ef24b168b065cf68
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-04 21:16:53 -0600

    feat mock: static DollarBill to initialize mock list

---
+++ xf.exercise.atm/Mocks/DollarsBillMock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using xf.exercise.atm.Models;
+
+namespace xf.exercise.atm.Mocks
+{
+    public static class DollarsBillMock
+    {
+        public static List<DollarsBill> GetDollarsBills()
+        {
+            var listDollarBills = new List<DollarsBill>();
+
+            listDollarBills.Add(new DollarsBill { Value = 100, Quantity = 10 });
+            listDollarBills.Add(new DollarsBill { Value = 50, Quantity = 10 });
+            listDollarBills.Add(new DollarsBill { Value = 20, Quantity = 10 });
+            listDollarBills.Add(new DollarsBill { Value = 10, Quantity = 10 });
+            listDollarBills.Add(new DollarsBill { Value = 5, Quantity = 10 });
+            listDollarBills.Add(new DollarsBill { Value = 1, Quantity = 10 });
+
+            return listDollarBills;
+        }
+    }
+}
