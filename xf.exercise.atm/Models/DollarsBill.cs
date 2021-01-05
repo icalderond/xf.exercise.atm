1f27ca280f11dffa9f580bf8bc6616a8fce41cac
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-04 21:15:51 -0600

    feat model: add dollarBill model to manage dollars

---
+++ xf.exercise.atm/Models/DollarsBill.cs
@@ -0,0 +1,18 @@
+﻿namespace xf.exercise.atm.Models
+{
+    /// <summary>
+    /// This class is representation of how much dollars bill there is in bank.
+    /// </summary>
+    public class DollarsBill
+    {
+        /// <summary>
+        /// Values of dollar
+        /// </summary>
+        public int Value { get; set; }
+
+        /// <summary>
+        /// Quantity of bills with the same value
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
