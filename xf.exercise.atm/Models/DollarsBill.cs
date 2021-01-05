2f20bb49848189310ad5a4e67620254adf410612
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-04 22:39:58 -0600

    feat model: add notify to change values

--- xf.exercise.atm/Models/DollarsBill.cs
+++ xf.exercise.atm/Models/DollarsBill.cs
@@ -1,18 +1,30 @@
-﻿namespace xf.exercise.atm.Models
+﻿using xf.exercise.atm.ViewModels;
+
+namespace xf.exercise.atm.Models
 {
     /// <summary>
     /// This class is representation of how much dollars bill there is in bank.
     /// </summary>
-    public class DollarsBill
+    public class DollarsBill : NotificationEnabledObject
     {
         /// <summary>
         /// Values of dollar
         /// </summary>
-        public int Value { get; set; }
+        private int _Value;
+        public int Value
+        {
+            get => _Value;
+            set => Set(ref _Value, value);
+        }
 
         /// <summary>
         /// Quantity of bills with the same value
         /// </summary>
-        public int Quantity { get; set; }
+        private int _Quantity;
+        public int Quantity
+        {
+            get => _Quantity;
+            set => Set(ref _Quantity, value);
+        }
     }
 }
