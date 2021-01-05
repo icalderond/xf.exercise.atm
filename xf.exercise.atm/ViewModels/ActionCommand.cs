767ff6dcfbeda25e5ef6f72a1670f11b4b7d42af
Israel Calderón de la Cruz <icalderond@outlook.com>
2021-01-04 21:19:53 -0600

    feat actionCommand: add flavor to implement iCommand

--- xf.exercise.atm/ViewModels/ActionCommand.cs
+++ xf.exercise.atm/ViewModels/ActionCommand.cs
@@ -15,4 +15,17 @@ namespace xf.exercise.atm.ViewModels
 
         public void Execute(object parameter) => action((T)parameter);
     }
+
+    public class ActionCommand : ICommand
+    {
+        readonly Action action;
+
+        public ActionCommand(Action action) => this.action = action;
+
+        public bool CanExecute(object parameter) => true;
+
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter) => action();
+    }
 }
